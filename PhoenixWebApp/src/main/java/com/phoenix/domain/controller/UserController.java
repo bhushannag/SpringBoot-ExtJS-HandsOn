47d9f2c8ed76a94458eeb51553f5a855b1ef81a3
Bhushan <bhushannag@cybage.com>
2023-01-25 20:04:17 +0530

    api updated

--- PhoenixWebApp/src/main/java/com/phoenix/domain/controller/UserController.java
+++ PhoenixWebApp/src/main/java/com/phoenix/domain/controller/UserController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,7 @@ import com.phoenix.domain.dto.UserRegistrationRequestDto;
 import com.phoenix.domain.dto.UserRegistrationResponseDto;
 import com.phoenix.domain.service.UserService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -25,7 +28,7 @@ public class UserController {
 	
 	@PreAuthorize(value="")
 	@PostMapping("/addUser")
-	public ResponseEntity<UserRegistrationResponseDto> userRegistration(UserRegistrationRequestDto userRegistrationRequestDto) throws FileUploadException{
+	public ResponseEntity<UserRegistrationResponseDto> userRegistration(@RequestBody UserRegistrationRequestDto userRegistrationRequestDto) throws FileUploadException{
 		System.out.println(userRegistrationRequestDto);
 		return new ResponseEntity<UserRegistrationResponseDto>(userService.addUser(userRegistrationRequestDto) , HttpStatus.CREATED);
 	}
